2438699eed195263369f43ef55fb72067569a641
Janette Griggs <janette.dev.code@gmail.com>
2017-09-25 09:25:25 -0700

    Display alerts and datetime

--- weather.py
+++ weather.py
@@ -1,4 +1,4 @@
-import json, re, requests, sys
+import datetime, json, pytz, re, requests, sys
 
 
 class ApiRequest:
@@ -21,27 +21,29 @@ class Geocode:
 
     @classmethod
     def get_geocode_json(cls, zip_code):
-        return ApiRequest.make_api_request(cls.get_googlemapsgeocoding_uri(),
-            {'components': 'postal_code:' + zip_code,
-            'key': Geocode.googlemapsgeocoding_api_key})
+        return ApiRequest.make_api_request(
+                cls.get_googlemapsgeocoding_uri(),
+                {'components': 'postal_code:' + zip_code,
+                'key': Geocode.googlemapsgeocoding_api_key}
+            )
 
 
 class Weather:
     darksky_api_key = '7f560844e81c036d5e8beafb449eda24'
 
-    def __init__(self, latitude=37.8267, longitude=-122.4233):
-        self.latitude = latitude
-        self.longitude = longitude
-
-    def get_darksky_uri(self):
+    @classmethod
+    def get_darksky_uri(cls, latitude, longitude):
         return ('https://api.darksky.net/forecast/'
             + Weather.darksky_api_key + '/'
-            + str(self.latitude) + ',' + str(self.longitude))
+            + str(latitude) + ',' + str(longitude))
 
-    def get_current_weather_json(self):
+    @classmethod
+    def get_current_weather_json(cls, latitude, longitude):
         exclude = 'minutely,hourly,daily,flags'
-        return ApiRequest.make_api_request(self.get_darksky_uri(),
-            {'exclude': exclude})
+        return ApiRequest.make_api_request(
+                cls.get_darksky_uri(latitude, longitude),
+                {'exclude': exclude}
+            )
 
 
 print("-----------------------------")
@@ -62,8 +64,7 @@ else:
     print("ZIP Code: {0}".format(zip_code))
     print("Latitude: {0}, Longitude: {1}".format(latitude, longitude))
 
-weather = Weather(latitude, longitude)
-current_weather_json = weather.get_current_weather_json()
+current_weather_json = Weather.get_current_weather_json(latitude, longitude)
 
 print()
 print("Current Weather for {0}".format(zip_code))
@@ -73,9 +74,45 @@ print()
 if 'error' in current_weather_json:
     print("Error! Could not retrieve current weather data.")
 else:
-    for k, v in current_weather_json['currently'].items():
-        label = k.replace(k[0], k[0].upper(), 1)
-        label = re.findall('[A-Z][^A-Z]*', label)
-        label = ' '.join(label) + ':'
-        value = v
-        print("{0:<30} {1:>15} ({2})".format(label, value, type(value)))
+    print("Latitude: {0}, Longitude: {1}".format(
+            current_weather_json['latitude'],
+            current_weather_json['longitude'])
+        )
+    timezone = current_weather_json['timezone']
+    tz = pytz.timezone(timezone)
+    print("Timezone: {0}".format(timezone))
+    if 'alerts' in current_weather_json:
+        for i, v in enumerate(current_weather_json['alerts'], start=1):
+            print('*' * 80)
+            print("Alert #{0}".format(i))
+            print(v['severity'].upper(), end=': ')
+            print(v['title'])
+            dt = datetime.datetime.fromtimestamp(v['time'])
+            dt_aware = tz.localize(dt)
+            print("Issued at: {:%Y-%m-%d %I:%M:%S %p %Z}".format(dt_aware))
+            dt = datetime.datetime.fromtimestamp(v['expires'])
+            dt_aware = tz.localize(dt)
+            print("Expires at: {:%Y-%m-%d %I:%M:%S %p %Z}".format(dt_aware))
+            print("Regions affected:", end=' ')
+            regions = v['regions']
+            regions_count = len(regions)
+            for i, r in enumerate(regions, start=1):
+                if i == regions_count:
+                    print(r)
+                else:
+                    print(r, end=', ')
+            print("Description:")
+            print(v['description'])
+        print('*' * 80)
+    if 'currently' in current_weather_json:
+        for k, v in current_weather_json['currently'].items():
+            label = k.replace(k[0], k[0].upper(), 1)
+            label = re.findall('[A-Z][^A-Z]*', label)
+            label = ' '.join(label) + ':'
+            if k == 'time':
+                dt = datetime.datetime.fromtimestamp(v)
+                dt_aware = tz.localize(dt)
+                print("Date: {:%Y-%m-%d}".format(dt_aware))
+                print("Time: {:%I:%M:%S %p %Z}".format(dt_aware))
+            else:
+                print("{0:<30} {1:>15} ({2})".format(label, v, type(v)))
