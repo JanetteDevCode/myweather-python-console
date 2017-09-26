8ca2c1c75cdb3443491c74dff32f3b8d01d0168c
Janette Griggs <janette.dev.code@gmail.com>
2017-09-26 11:08:40 -0700

    Fix timezone and separate weather alerts display into method

--- weather.py
+++ weather.py
@@ -19,46 +19,62 @@ class Weather:
         else:
             weather_timezone = timezone(weather_json['timezone'])
 
-            print()
-            print("Current Weather for {0}".format(self.__zip_code))
-            print("--------------------{0}".format('-' * len(self.__zip_code)))
-            print()
-            print("Latitude: {0}, Longitude: {1}".format(
-                    weather_json['latitude'],
-                    weather_json['longitude'])
-                )
-            print("Timezone:", weather_timezone.zone)
+            # print()
+            # print("Latitude: {0}, Longitude: {1}".format(
+            #         weather_json['latitude'],
+            #         weather_json['longitude'])
+            #     )
+            # print("Timezone:", weather_timezone.zone)
 
             if 'alerts' in weather_json:
-                for i, v in enumerate(weather_json['alerts'], start=1):
-                    print('*' * 80)
-                    print("Alert #{0}".format(i))
-                    print(v['severity'].upper(), end=': ')
-                    print(v['title'])
-                    alert_time = weather_timezone.localize(datetime.fromtimestamp(v['time']))
-                    print("Issued at: {:%Y-%m-%d %I:%M:%S %p %Z}".format(alert_time))
-                    alert_expires = weather_timezone.localize(datetime.fromtimestamp(v['expires']))
-                    print("Expires at: {:%Y-%m-%d %I:%M:%S %p %Z}".format(alert_expires))
-                    print("Regions affected:", end=' ')
-                    alert_regions = v['regions']
-                    alert_regions_count = len(alert_regions)
-                    for i, r in enumerate(alert_regions, start=1):
-                        if i == alert_regions_count:
-                            print(r)
-                        else:
-                            print(r, end=', ')
-                    print("Description:")
-                    print(v['description'], end='')
-                print('*' * 80)
+                self.display_alerts(weather_timezone, weather_json['alerts'])
 
             if 'currently' in weather_json:
+                print()
+                print("--------------------{0}".format('-' * len(self.__zip_code)))
+                print("Current Weather for {0}".format(self.__zip_code))
+                print("--------------------{0}".format('-' * len(self.__zip_code)))
+                print()
+
                 for k, v in weather_json['currently'].items():
                     label = k.replace(k[0], k[0].upper(), 1)
                     label = re.findall('[A-Z][^A-Z]*', label)
                     label = ' '.join(label) + ':'
                     if k == 'time':
-                        currently_time = weather_timezone.localize(datetime.fromtimestamp(v))
+                        currently_time = datetime.fromtimestamp(v, weather_timezone)
                         print("Date: {:%Y-%m-%d}".format(currently_time))
                         print("Time: {:%I:%M:%S %p %Z}".format(currently_time))
                     else:
                         print("{0:<30} {1:>15} ({2})".format(label, v, type(v)))
+
+    def display_alerts(self, timezone, alerts={}):
+        print()
+        print("-------------------{0}".format('-' * len(self.__zip_code)))
+        print("Weather Alerts for {0}".format(self.__zip_code))
+        print("-------------------{0}".format('-' * len(self.__zip_code)))
+        print()
+
+        for i, alert in enumerate(alerts, start=1):
+            print('*' * 80)
+            print("Alert #{0}".format(i))
+            print()
+            print(alert['severity'].upper(), end=': ')
+            print(alert['title'])
+            alert_time = datetime.fromtimestamp(alert['time'], timezone)
+            print("Issued at: {:%Y-%m-%d %I:%M:%S %p %Z}".format(alert_time))
+            alert_expires = datetime.fromtimestamp(alert['expires'], timezone)
+            print("Expires at: {:%Y-%m-%d %I:%M:%S %p %Z}".format(alert_expires))
+            print()
+            print("Regions affected:", end=' ')
+            alert_regions = alert['regions']
+            alert_regions_count = len(alert_regions)
+            for i, region in enumerate(alert_regions, start=1):
+                if i == alert_regions_count:
+                    print(region)
+                else:
+                    print(region, end=', ')
+            print()
+            print("Description:")
+            print(alert['description'])
+            print("Additional Info: {0}".format(alert['uri']))
+        print('*' * 80)
